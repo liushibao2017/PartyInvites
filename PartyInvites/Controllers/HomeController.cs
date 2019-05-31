e697d2e75beb0e2adc51218b698cc29700c70005
诗保 刘 <1151702271@qq.com>
2019-05-31 13:23:07 +0800

    创建一个简单的数据录入应用程序

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PartyInvites.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,29 @@ namespace PartyInvites.Controllers
         // GET: Home
         public ViewResult Index()
         {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning " : "Good Afternoon";
             return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO;对晚会的组织者发送Email响应
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                //有验证错误
+                return View();
+            }
         }
     }
 }
